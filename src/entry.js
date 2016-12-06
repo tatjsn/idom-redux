31586dfcca2615a16e4fb40657341dd23210d9a6
Taz Jackson <tatjsn@users.noreply.github.com>
2016-12-07 00:53:43 +0900

    Curry all render funcs being patched

--- src/entry.js
+++ src/entry.js
@@ -2,7 +2,7 @@
 import { patch } from 'incremental-dom';
 import './elements';
 
-const render = ({ greet, time, ontap }) => (
+const render = ({ greet, time, ontap }) => () => (
   <div>
     <h1>{greet}</h1>
     <x-sub-module
@@ -19,25 +19,17 @@ const render = ({ greet, time, ontap }) => (
 );
 
 const app = document.getElementById('app');
-patch(app, () => {
-  const greet = 'Hi from incremental-dom';
-  const time = (new Date()).toISOString();
-  render({
-    greet,
-    time,
-    ontap: () => window.alert(greet),
-  });
-});
+patch(app, render({
+  greet: 'Hi from incremental-dom',
+  time: (new Date()).toISOString(),
+  ontap: () => window.alert('Hi'),
+}));
 
 const updateButton = document.getElementById('update');
 updateButton.onclick = () => {
-  patch(app, () => {
-    const greet = 'Hello again';
-    const time = (new Date()).toISOString();
-    render({
-      greet,
-      time,
-      ontap: () => window.alert(greet),
-    });
-  });
+  patch(app, render({
+    greet: 'Hi again',
+    time: (new Date()).toISOString(),
+    ontap: () => window.alert('Hi 2nd time'),
+  }));
 };
